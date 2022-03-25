64bd6c5aa7171036f51dfbac4fe40e9c266f7ab9
Serdar <serdar.onculer.work@hotmail.com>
2022-03-25 18:35:53 +0300

    Average Widget added

---
+++ lib/widgets/show_average.dart
@@ -0,0 +1,32 @@
+import 'package:calculating_grade_point_average/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class ShowAverage extends StatelessWidget {
+  final double average;
+  final int lessonsNumber;
+  const ShowAverage(
+      {Key? key, required this.average, required this.lessonsNumber})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          lessonsNumber > 0 ? '$lessonsNumber lessons added' : 'Ders Seçiniz',
+          style: AppConstants.styles(fontWeight: FontWeight.w600, fontSize: 16),
+        ),
+        Text(
+          average > 0 ? '${average.toStringAsFixed(2)}' : '0.00',
+          style: AppConstants.styles(fontSize: 55, fontWeight: FontWeight.w800),
+        ),
+        Text(
+          "Average",
+          style: AppConstants.styles(fontWeight: FontWeight.w600, fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
