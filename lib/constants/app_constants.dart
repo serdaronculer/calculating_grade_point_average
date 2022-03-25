e223468cfd09a82936b5db4003e686f870bd3e88
Serdar <serdar.onculer.work@hotmail.com>
2022-03-25 18:36:47 +0300

    updates

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -10,11 +10,31 @@ class AppConstants {
   /// [Text] Constants
   static const String title = "Calculate Average";
 
+  /// [BorderRadius]
+  static final BorderRadius borderRadius20 = BorderRadius.circular(20);
+
   /// [TextStyle] Constants
+
+  static TextStyle styles(
+      {double fontSize = 16.00,
+      FontWeight fontWeight = FontWeight.w500,
+      Color color = primaryColor}) {
+    return GoogleFonts.quicksand(
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
 
   static final TextStyle headlineStyle = GoogleFonts.quicksand(
     fontSize: 24,
     fontWeight: FontWeight.w900,
+    color: primaryColor,
+  );
+
+  static final TextStyle averageStyle = GoogleFonts.quicksand(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
     color: primaryColor,
   );
 }
